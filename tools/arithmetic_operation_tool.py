00f76434b2f48e15a921777fee6f6883a0ae25c6
Adonpm <adon.pmpm@gmail.com>
2025-07-11 23:27:07 +0530

    Added "add", "multiply" & "currency_converter"

---
+++ tools/arithmetic_operation_tool.py
@@ -0,0 +1,42 @@
+import os
+from langchain.tools import tool
+from langchain_community.utilities.alpha_vantage import AlphaVantageAPIWrapper
+from dotenv import load_dotenv
+load_dotenv()
+
+@tool
+def multiply(a:int, b:int) -> int:
+    '''
+    Multiply two integers
+    
+    Args:
+        a (int): First integer
+        b (int): Second integer
+
+    Returns:
+        int: The product of a and b
+    '''
+    return a * b
+
+@tool
+def add(a:int, b:int) -> int:
+    '''
+    Add two integers
+    
+    Args:
+        a (int): First integer
+        b (int): Second integer
+
+    Returns:
+        int: Sum of a and b
+    '''
+    return a + b
+
+@tool
+def currency_converter(from_curr:str, to_curr:str, value:float) -> float:
+    '''Converts from_curr to to_curr'''
+    os.environ["ALPHAVANTAGE_API_KEY"] = os.getenv("ALPHAVANTAGE_API_KEY")
+    alpha_vantage = AlphaVantageAPIWrapper()
+    response = alpha_vantage._get_exchange_rate(from_curr, to_curr)
+    exchange_rate = response["Realtime Currency Exchange Rate"]['5. Exchange Rate']
+    return value * float(exchange_rate)
