833abee911a0194d56e34691b7cd2caf034d36f2
Adonpm <adon.pmpm@gmail.com>
2025-07-12 14:08:56 +0530

    fix

--- agent/agentic_workflow.py
+++ agent/agentic_workflow.py
@@ -19,15 +19,15 @@ class GraphBuilder:
         self.calculator_tools = CalculatorTool()
         self.currency_converter_tools = CurrencyConversionTool()
 
-        self.tools.append([*self.weather_tools.weather_tool_list, # * : for unpacking the list
+        self.tools.extend([*self.weather_tools.weather_tool_list, # * : for unpacking the list
                            *self.place_search_tools.place_search_tool_list,
                            *self.calculator_tools.calculator_tool_list,
                            *self.currency_converter_tools.currency_converter_tool_list])
-
+        
         self.llm_with_tools = self.llm.bind_tools(tools=self.tools)
 
         self.graph = None 
-
+        
         self.system_prompt = SYSTEM_PROMPT
         
     def agent_function(self, state:MessagesState):
